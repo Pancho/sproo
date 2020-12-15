120dd92745695db606a39b7793318ea67c42da9e
Danijel Pančić <danijel.pancic@gmail.com>
2020-12-15 01:17:06 +0100

    missing files

---
+++ workarea/app/pages/login/login.js
@@ -0,0 +1,23 @@
+import Component from '../../../fiu/js/component.js';
+
+export default class EmptyPageComponent extends Component {
+	static tagName = 'empty-page';
+	static template = '/app/pages/login/login';
+	static stylesheets = [
+		'/fiu/css/meta',
+		'/fiu/css/normalize',
+		'/app/pages/login/login',
+	];
+	static registerComponents = [
+		'/baza/form/form.js',
+		'/baza/field/field.js',
+	];
+
+	form;
+
+	onTemplateLoaded() {
+		this.form.setSuccessHandler(response => {
+			console.log(response);
+		});
+	}
+}
