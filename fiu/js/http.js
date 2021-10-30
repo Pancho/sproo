38d856f611030de90bb5b10463214046b054bb61
Danijel Pančić <danijel.pancic@gmail.com>
2021-10-30 05:16:45 +0200

    made http async so one can await calls

--- fiu/js/http.js
+++ fiu/js/http.js
@@ -23,7 +23,111 @@ export default class Http {
 		this.authentication = authentication;
 	}
 
-	constructUrl(path, params) {
+	// MDN: The GET method requests a representation of the specified resource. Requests using GET should only retrieve data.
+	async get(path, params, httpHeaders, authenticate = true) {
+		const headers = {
+				...Http.STANDARD_HEADERS,
+				...httpHeaders,
+			},
+			options = {
+				method: 'GET',
+				headers: headers,
+			},
+			url = Http.constructUrl(path, params);
+
+		return await this.fetch(url, options, authenticate);
+	}
+
+	// MDN: The HEAD method asks for a response identical to that of a GET request, but without the response body.
+	async head(path, params, httpHeaders, authenticate = true) {
+		const headers = {
+				...Http.STANDARD_HEADERS,
+				...httpHeaders,
+			},
+			options = {
+				method: 'HEAD',
+				headers: headers,
+			},
+			url = Http.constructUrl(path, params);
+
+		return await this.fetch(url, options, authenticate);
+	}
+
+	// MDN: The DELETE method deletes the specified resource.
+	async delete(path, params, httpHeaders, authenticate = true) {
+		const headers = {
+				...Http.STANDARD_HEADERS,
+				...httpHeaders,
+			},
+			options = {
+				method: 'DELETE',
+				headers: headers,
+			},
+			url = Http.constructUrl(path, params);
+
+		return await this.fetch(url, options, authenticate);
+	}
+
+	// MDN: The OPTIONS method is used to describe the communication options for the target resource.
+	async options(path, params, httpHeaders, authenticate = true) {
+		const headers = {
+				...Http.STANDARD_HEADERS,
+				...httpHeaders,
+			},
+			options = {
+				method: 'OPTIONS',
+				headers: headers,
+			},
+			url = Http.constructUrl(path, params);
+
+		return await this.fetch(url, options, authenticate);
+	}
+
+	/* MDN: The POST method is used to submit an entity to the specified resource, often causing a change in state or side effects on the
+	* server.
+	*/
+	async post(path, body, fetchOptions, httpHeaders, authenticate = true) {
+		const headers = {
+				...Http.STANDARD_HEADERS,
+				...httpHeaders,
+			},
+			options = {
+				method: 'POST',
+				headers: headers,
+				body: body,
+				...fetchOptions,
+			},
+			url = Http.constructUrl(path);
+
+		return await this.fetch(url, options, authenticate);
+	}
+
+	// MDN: The PUT method replaces all current representations of the target resource with the request payload.
+	async put(path, body, fetchOptions, httpHeaders, authenticate = true) {
+		const headers = {
+				...Http.STANDARD_HEADERS,
+				...httpHeaders,
+			},
+			options = {
+				method: 'PUT',
+				headers: headers,
+				body: body,
+				...fetchOptions,
+			},
+			url = Http.constructUrl(path);
+
+		return await this.fetch(url, options, authenticate);
+	}
+
+	async fetch(url, options, authenticate) {
+		if (authenticate) {
+			return await window.fetch(url, this.authentication.addAuthentication(options));
+		}
+
+		return await window.fetch(url, options);
+	}
+
+	static constructUrl(path, params) {
 		const localPath = path.startsWith('/') ? path.slice(1) : path;
 		let queryString = '';
 
@@ -36,109 +140,5 @@ export default class Http {
 		}
 
 		return `${ this.httpEndpointStub }/${ localPath }`;
-	}
-
-	// MDN: The GET method requests a representation of the specified resource. Requests using GET should only retrieve data.
-	get(path, params, httpHeaders, authenticate = true) {
-		const headers = {
-				...Http.STANDARD_HEADERS,
-				...httpHeaders,
-			},
-			options = {
-				method: 'GET',
-				headers: headers,
-			},
-			url = this.constructUrl(path, params);
-
-		return this.fetch(url, options, authenticate);
-	}
-
-	// MDN: The HEAD method asks for a response identical to that of a GET request, but without the response body.
-	head(path, params, httpHeaders, authenticate = true) {
-		const headers = {
-				...Http.STANDARD_HEADERS,
-				...httpHeaders,
-			},
-			options = {
-				method: 'HEAD',
-				headers: headers,
-			},
-			url = this.constructUrl(path, params);
-
-		return this.fetch(url, options, authenticate);
-	}
-
-	// MDN: The DELETE method deletes the specified resource.
-	delete(path, params, httpHeaders, authenticate = true) {
-		const headers = {
-				...Http.STANDARD_HEADERS,
-				...httpHeaders,
-			},
-			options = {
-				method: 'DELETE',
-				headers: headers,
-			},
-			url = this.constructUrl(path, params);
-
-		return this.fetch(url, options, authenticate);
-	}
-
-	// MDN: The OPTIONS method is used to describe the communication options for the target resource.
-	options(path, params, httpHeaders, authenticate = true) {
-		const headers = {
-				...Http.STANDARD_HEADERS,
-				...httpHeaders,
-			},
-			options = {
-				method: 'OPTIONS',
-				headers: headers,
-			},
-			url = this.constructUrl(path, params);
-
-		return this.fetch(url, options, authenticate);
-	}
-
-	/* MDN: The POST method is used to submit an entity to the specified resource, often causing a change in state or side effects on the
-	* server.
-	*/
-	post(path, body, fetchOptions, httpHeaders, authenticate = true) {
-		const headers = {
-				...Http.STANDARD_HEADERS,
-				...httpHeaders,
-			},
-			options = {
-				method: 'POST',
-				headers: headers,
-				body: body,
-				...fetchOptions,
-			},
-			url = this.constructUrl(path);
-
-		return this.fetch(url, options, authenticate);
-	}
-
-	// MDN: The PUT method replaces all current representations of the target resource with the request payload.
-	put(path, body, fetchOptions, httpHeaders, authenticate = true) {
-		const headers = {
-				...Http.STANDARD_HEADERS,
-				...httpHeaders,
-			},
-			options = {
-				method: 'PUT',
-				headers: headers,
-				body: body,
-				...fetchOptions,
-			},
-			url = this.constructUrl(path);
-
-		return this.fetch(url, options, authenticate);
-	}
-
-	fetch(url, options, authenticate) {
-		if (authenticate) {
-			return window.fetch(url, this.authentication.addAuthentication(options));
-		}
-
-		return window.fetch(url, options);
 	}
 }
