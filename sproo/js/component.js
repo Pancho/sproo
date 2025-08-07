3ea51e0602824d461f00e67491f80e64a63fc1c5
Danijel Pančić <danijel.pancic@gmail.com>
2025-08-07 07:20:54 +0200

    Passing the object as the correct property helps a lot

--- sproo/js/component.js
+++ sproo/js/component.js
@@ -342,7 +342,7 @@ export default class Component extends HTMLElement {
         this.dispatchEvent(new CustomEvent(eventName, {
                 bubbles: true,
                 composed: true,
-                payload: obj,
+                detail: obj,
             },
         ));
     }
